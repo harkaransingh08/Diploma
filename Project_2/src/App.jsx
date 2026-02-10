c71a7c1abd1ecc2fc539e330f7ccf14da8a298a2
karan <harkaransingh08ktl@gmail.com>
2026-02-11 00:05:24 +0530

    ui

--- Project_2/src/App.jsx
+++ Project_2/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home/Home'
+import Navbar from './components/Navbar.jsx'
+import Home from './components/Home/Home.jsx'
 import PageNotFound from './components/PageNotFound.jsx'
 import Footer from './components/Footer.jsx'
 import Service from './components/Service.jsx'
