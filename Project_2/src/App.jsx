d895cc146446cee4ab506afcb22b3962e1154ebc
karan <harkaransingh08ktl@gmail.com>
2026-02-11 00:00:43 +0530

    dsadas

--- Project_2/src/App.jsx
+++ Project_2/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import Home from './components/home/Home.jsx'
+import Navbar from './components/Navbar_fix'
+import Home from './components/Home_fix/Home'
 import PageNotFound from './components/PageNotFound.jsx'
 import Footer from './components/Footer.jsx'
 import Service from './components/Service.jsx'
